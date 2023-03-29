fab0358856e211a267548d4847ed7f7cc0f1106a
shobola faruq <shobolafaruq@gmail.com>
2023-03-29 14:08:38 +0100

    Delete todos

--- Todo List/app.js
+++ Todo List/app.js
@@ -23,4 +23,13 @@ addForm.addEventListener('submit', e => {
         addForm.reset();
     }
 
+}); 
+
+// delete todos
+list.addEventListener('click', e => {
+
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove()
+    }
+
 });
